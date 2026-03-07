dd0ee00dcb36680c29cc8b212ae40f1186616780
aduch123 <aduchmainad@gmail.com>
2026-03-07 14:49:35 +0300

    UI finished.
     I have modified all three files to create my calculator UI.

---
+++ script.js
@@ -0,0 +1,49 @@
+function createButtons() {
+    const nums = document.getElementById("numbers");
+    const ops = document.getElementById("operators");
+
+    //Numbers
+    for(let i = 0; i < 12; ++i) {
+        const num = document.createElement("div");
+        const p = document.createElement("p");
+        num.addEventListener("click", displayContent);
+
+        num.classList = "num";
+        if(i < 9) p.innerText = i + 1;
+        else {
+            switch(i) {
+                case 9:
+                    p.innerText = "Clear";
+                    p.style.fontSize = "16px";
+                    break;
+                case 10:
+                    p.innerText = 0;
+                    break;
+                case 11:
+                    p.innerText = "Delete";
+                    p.style.fontSize = "16px";
+                    break;
+            }
+        }
+
+        num.appendChild(p);
+        nums.appendChild(num);
+    }
+
+    //operators
+    for(let i = 0; i < 5; ++i) {
+        let operators = ["+", "-", "×", "/", "="];
+        const op = document.createElement("div");
+        const p = document.createElement("p");
+
+        op.classList = "op";
+        p.innerText = operators[i];
+        op.addEventListener("click", displayContent);
+
+        op.appendChild(p);
+        ops.appendChild(op);
+    }
+}
+
+createButtons();
+
